c77337ce3dfdb6ec5ae77f4b9712253645530552
miriamschaefer <msuarezmelian@gmail.com>
2020-06-30 09:52:30 +0200

    attempts counter

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const attemptsAlert = document.querySelector('.js-attempts');
 
 //intentos
 
-let attempts = {};
+let attempts = 0;
 
 //número aleatorio
 
@@ -18,25 +18,23 @@ console.log(`Tu número aleatorio es ${randomNumber}`);
 
 //funciones
 
-// esta coge el número del usuario y lo imprime en la consola
-function getUserNumber() {
-  const userNumber = userNumberInput.value;
-  console.log(`El número que elige el usuario es ${userNumber}`);
-}
-
 function compareNumber() {
   const userNumber = userNumberInput.value;
-  let attemptsCheck = (attempts += 1);
   console.log(userNumber);
 
+  let attemptsCheck = (attempts += 1);
+  attemptsAlert.innerHTML = `${attemptsCheck} intentos`;
+  console.log(attemptsCheck);
+
   if (userNumber === randomNumber) {
-    return console.log("It's a match!");
+    console.log("It's a match!");
     clue.innerHTML = '¡Has ganado campeona!';
   } else if (userNumber <= randomNumber) {
-    return console.log('Es menor');
+    console.log('Es menor');
     clue.innerHTML = '¡Demasiado bajo!';
   } else if (userNumber >= randomNumber) {
-    return console.log('Es mayor');
+    console.log('Es mayor');
+    clue.innerHTML = '¡Demasiado alto!';
   }
 }
 
