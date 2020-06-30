4514f2c38ce1ca37916f6a2c45ae1964e1e0bc60
miriamschaefer <msuarezmelian@gmail.com>
2020-06-30 10:01:31 +0200

    fix conditional

--- js/main.js
+++ js/main.js
@@ -20,21 +20,24 @@ console.log(`Tu número aleatorio es ${randomNumber}`);
 
 function compareNumber() {
   const userNumber = userNumberInput.value;
-  console.log(userNumber);
+  console.log(`El número del usuario es ${userNumber}`);
 
   let attemptsCheck = (attempts += 1);
   attemptsAlert.innerHTML = `${attemptsCheck} intentos`;
   console.log(attemptsCheck);
 
-  if (userNumber === randomNumber) {
-    console.log("It's a match!");
-    clue.innerHTML = '¡Has ganado campeona!';
-  } else if (userNumber <= randomNumber) {
+  if (userNumber < 0) {
+    console.log('El número debe estar entre 1 y 100.');
+    clue.innerHTML = 'El número debe estar entre 1 y 100.';
+  } else if (userNumber < randomNumber) {
     console.log('Es menor');
     clue.innerHTML = '¡Demasiado bajo!';
-  } else if (userNumber >= randomNumber) {
+  } else if (userNumber > randomNumber) {
     console.log('Es mayor');
     clue.innerHTML = '¡Demasiado alto!';
+  } else {
+    console.log("It's a match!");
+    clue.innerHTML = '¡Has ganado campeona!';
   }
 }
 
